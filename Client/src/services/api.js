e54bf84867e72112acdc7c100f4f4c4d953dfaca
Sreya u <sreyau0@gmail.com>
2026-07-17 19:59:19 +0530

    Add axios auth interceptor

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,19 @@
+
+
 import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://hospital-appointment-system-1-1euy.onrender.com/api",
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default API;
