0df60d9492ae2f19c1d01d876439f90866e8a174
Sreya u <sreyau0@gmail.com>
2026-07-19 10:46:23 +0530

    Fixed forgot password OTP and password reset

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hospital-appointment-system-1-1euy.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 API.interceptors.request.use((config) => {
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,20 +1,29 @@
 const nodemailer = require("nodemailer");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
 const sendEmail = async (to, subject, text) => {
+  console.log("Creating transporter...");
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    family: 4,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  console.log("Sending email to:", to);
+
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
-    to: to,
-    subject: subject,
-    text: text,
+    to,
+    subject,
+    text,
   });
+
+  console.log("Email sent successfully");
 };
 
 module.exports = sendEmail;
