9700f9afabf800c301446565a1440da96b6b66a2
Sreya u <sreyau0@gmail.com>
2026-07-19 13:57:21 +0530

    Fix Gmail OTP email sending

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,29 +1,30 @@
+
+
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text) => {
+
   console.log("Creating transporter...");
+  console.log("Sending email to:", to);
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
+      pass: process.env.EMAIL_PASS
+    }
   });
 
-  console.log("Sending email to:", to);
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
     to,
     subject,
-    text,
+    text
   });
 
   console.log("Email sent successfully");
+
 };
 
 module.exports = sendEmail;
